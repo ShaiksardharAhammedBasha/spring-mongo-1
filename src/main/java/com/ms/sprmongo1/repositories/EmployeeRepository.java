b29e6f89bd1e60d55b9649d4b553de6f0d87dd4b
ShaikSardharAhammedBasha <smabasha222@gmail.com>
2024-03-07 15:34:42 +0530

    reverted back to integer

--- src/main/java/com/ms/sprmongo1/repositories/EmployeeRepository.java
+++ src/main/java/com/ms/sprmongo1/repositories/EmployeeRepository.java
@@ -4,8 +4,6 @@ import com.ms.sprmongo1.models.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Formatter;
-
 @Repository
-public interface EmployeeRepository extends MongoRepository<Employee, Formatter.BigDecimalLayoutForm> {
+public interface EmployeeRepository extends MongoRepository<Employee, Integerg> {
 }
