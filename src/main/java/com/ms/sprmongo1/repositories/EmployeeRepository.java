7faa7ed1855fb9d18dfdb122a0a4970df0ede87f
ShaikSardharAhammedBasha <smabasha222@gmail.com>
2024-03-07 15:21:39 +0530

    removed unused imports in repo class/inteface

--- src/main/java/com/ms/sprmongo1/repositories/EmployeeRepository.java
+++ src/main/java/com/ms/sprmongo1/repositories/EmployeeRepository.java
@@ -4,8 +4,6 @@ import com.ms.sprmongo1.models.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigInteger;
-
 @Repository
 public interface EmployeeRepository extends MongoRepository<Employee, Integer> {
 }
